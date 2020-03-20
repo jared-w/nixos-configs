190e9a1731c76a2bc95b556caa957c19e4bdc1eb
jared-w <jaredweakly@gmail.com>
2020-03-19 23:14:13 -0700

    Build expensive nixos configuration outputs with CI

---
+++ cachix/jared-w-nixos-configs.nix
@@ -0,0 +1,12 @@
+
+{
+  nix = {
+    binaryCaches = [
+      "https://jared-w-nixos-configs.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "jared-w-nixos-configs.cachix.org-1:TIvgvGRdbc2DhxoKYZtpdP8JKT91OhIAxhSefxXYruA="
+    ];
+  };
+}
+    
